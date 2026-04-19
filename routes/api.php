c116845e33a3ad0221edc84da9e1b8c3bb8e7375
Naser Man <tgza54@gmail.com>
2026-04-19 12:52:57 +0300

    Profile Controller update

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\API\AddressController;
-use App\Http\Controllers\Api\Admin\AdminProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AddressController;
+use App\Http\Controllers\Api\Admin\AdminProductController;
 use App\Http\Controllers\Api\Admin\AdminDashboardController;
 use App\Http\Controllers\Api\Admin\AdminOrderController;
 use App\Http\Controllers\Api\AuthController;
