25dd8f2100589e852e65838b89848a7433735eb5
Naser Man <tgza54@gmail.com>
2026-04-22 00:59:22 +0300

    Add wishlist pages && checkout

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -49,7 +49,7 @@ class CheckoutController extends Controller
 
     $data['customer_name'] = $address->name;
     $data['shipping_address'] = $address->address;
-    $data['customer_phone'] = $address->customer_phone;
+    $data['customer_phone'] = $address->customer_phone ?? $user->phone;
     $data['shipping_city'] = $address->city;
     $data['shipping_postal_code'] = $address->postal_code;
     }
