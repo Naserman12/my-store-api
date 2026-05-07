a6f3187849eeddf0f49126ccd688bca5353f2916
Naser Man <tgza54@gmail.com>
2026-05-07 21:07:17 +0300

    upload Images Cloudinary

--- app/helpers.php
+++ app/helpers.php
@@ -1,11 +1,13 @@
 <?php
 use App\Models\Notification;
 
-function sendNotification($userId, $title, $message)
-{
-    Notification::create([
-        'user_id' => $userId,
-        'title' => $title,
-        'message' => $message
-    ]);
+if (!function_exists('sendNotification')) {
+    function sendNotification($userId, $title, $message)
+    {
+        Notification::create([
+            'user_id' => $userId,
+            'title' => $title,
+            'message' => $message,
+        ]);
+    }
 }
